7118bf952e7f8a9a24c3403517ea9e64c8422160
chiu <chiupao8@gmail.com>
2024-08-20 11:22:18 +0800

    Add simulation files

---
+++ test/Data_generation_asar.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import numpy as np
+import os
+from numpy import linalg as LA
+
+
+def generate_asar_data(n, p, rho2, seed, setting_index, save_file=False):
+    """
+    Input
+    n               : Number of data
+    p               : Number of features
+    rho2            : Degree of correlation
+    seed            : Random seed
+    setting_index   : Number of the experiments
+    save_file       : Whether to save the generated data
+
+    Output
+    X         : Feature
+    y         : Target
+    beta      : True beta
+    record    : The detail information in each dataset (n, p, rho^2, eigenvalue, and correlation matrix)
+
+    Source:
+    Yasin Asar. (2017). Some new methods to solve multicollinearity in logistic regression.
+    Communications in Statistics - Simulation and Computation, 46:4, 2576-2586. Section 3.1 (page 2581)
+    """
+
+    # Set random seeds for reproducibility
+    rng = np.random.default_rng(seed)
+
+    # Generate X: n * (p + 1) matrix
+    X = np.zeros((n, p + 1))
+    for i in range(n):
+        z_ip = rng.normal(loc=0, scale=1)
+        for j in range(p + 1):
+            z_ij = rng.normal(loc=0, scale=1)
+            X[i, j] = np.sqrt(1 - rho2) * z_ij + np.sqrt(rho2) * z_ip
+
+    # Calculate the correlation matrix
+    correlation_matrix = np.corrcoef(X, rowvar=False)
+
+    # Generate beta: (p + 1) * 1 coefficient vector
+    beta = rng.standard_normal(p + 1)
+    # Normalize beta to ensure β'β = 1
+    beta = beta / LA.norm(beta)
+    # Check if β'β = 1
+    beta_norm = LA.norm(beta)
+    assert np.isclose(beta_norm, 1), f"β'β is not equal to 1, got {beta_norm}"
+
+    # Generate P:
+    P = np.zeros(n)
+    P = np.exp(X @ beta) / (1 + np.exp(X @ beta))
+
+    # Generate y:
+    y = rng.binomial(1, P)
+
+    # Generate W_hat:
+    W_hat = np.diag(P * (1 - P))
+
+    # Calculate XtWhX
+    Xt_Wh_X = X.T @ W_hat @ X
+    eigenvalues, _ = LA.eigh(Xt_Wh_X)
+
+    # Save the data
+    if save_file == True:
+        output_dir = f"dataset/Asar/"
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir, exist_ok=True)
+
+        data = pd.DataFrame(X)
+        data["y"] = y
+        data.to_csv(f"{output_dir}/Data_{setting_index + 1}.csv", index=False)
+
+    return (
+        X,
+        y,
+        beta,
+        {
+            "n": n,
+            "p": p,
+            "rho^2": rho2,
+            "eigenvalue": eigenvalues,
+            "correlation matrix": correlation_matrix,
+        },
+    )
+
+
+"""
+# Settings for each dataset in Table 1 to Table 4 (page 2581)
+n_values = [50, 100, 200]
+p_values = [4, 8]
+rho2_values = [0.90, 0.95, 0.99]
+base_seed = 426
+rng_seed_generator = np.random.default_rng(base_seed)
+seeds = rng_seed_generator.integers(low=0, high=50000, size=50000)
+
+# Start simulation
+record = []
+setting_index = 0
+for p in p_values:
+    for rho2 in rho2_values:
+        for n in n_values:
+            current_seed = seeds[setting_index]
+            X, y, beta, result = generate_asar_data(
+                n,
+                p,
+                rho2,
+                seed=current_seed,
+                setting_index=setting_index,
+                save_file=True,
+            )
+            record.append(result)
+            setting_index += 1
+
+record_df = pd.DataFrame(record)
+record_df.to_excel("dataset/Asar/record.xlsx", index=False)
+"""
---
+++ test/MLE_asar.py
@@ -0,0 +1,146 @@
+import os
+import numpy as np
+import pandas as pd
+from numpy import linalg as LA
+from Data_generation_asar import generate_asar_data
+
+
+def MLE(X, y, tol, setting_index, save_file=False):
+    """
+    Input
+    X               : Feature
+    y               : Target
+    tol             : Tolerance for convergence
+    setting_index   : Number of the experiments
+    save_file       : Whether to save the computation result of MLE
+
+    Output
+    beta_MLE      : The optimal beta from maximum likelihood esitmator (MLE)
+    X_TWX         : The marix X^T @ W @ X
+
+    Source:
+    Murphy-Probabilistic Machine Learning-book1
+    Page 346, Algorithm 10.1, Iteratively reweighted least squares (IRLS)
+    """
+    n, p = X.shape
+    beta = np.zeros(p)
+    converge = np.inf
+
+    eigh_val_rec, determinant_rec = [], []
+
+    iteration = 0
+    while converge > tol:
+        old_beta = np.copy(beta)
+        a = X @ beta
+        P = 1 / (1 + np.exp(-a))
+
+        if np.any(P <= 0) or np.any(P >= 1):
+            # print(f"Iteration {iteration}: P contains invalid values, break iteration.")
+            break
+
+        z = np.log(P) + (y - P) / (P * (1 - P))
+        W = np.diag(P * (1 - P))
+
+        X_TWX = X.T @ W @ X
+        beta = LA.inv(X_TWX) @ X.T @ W @ z
+        converge = LA.norm(old_beta - beta)
+
+        eigenvalues, _ = LA.eigh(X_TWX)
+        determinant = LA.det(X_TWX)
+        eigh_val_rec.append(eigenvalues)
+        determinant_rec.append(determinant)
+
+        iteration += 1
+
+        # Print debugging information
+        # print(
+        #    f"Iteration {iteration}: \nConvergence = {converge}, \nBeta_MLE = {beta}\n"
+        # )
+
+    beta_MLE = beta
+
+    if save_file == True:
+        data = {
+            "Iteration": [i for i in range(iteration)],
+            "Eigenvalues": eigh_val_rec,
+            "Determinant": determinant_rec,
+        }
+        df = pd.DataFrame(data)
+
+        # Save the information data
+        output_dir = f"output/Asar_iteration_info/"
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir, exist_ok=True)
+
+        filename = f"info_{setting_index + 1}.xlsx"
+        filepath = os.path.join(output_dir, filename)
+
+        with pd.ExcelWriter(filepath, engine="xlsxwriter") as writer:
+            df.to_excel(writer, index=False)
+
+            # Adjust column widths
+            worksheet = writer.sheets["Sheet1"]
+            for i, col in enumerate(df.columns):
+                column_len = max(df[col].astype(str).map(len).max(), len(col))
+                worksheet.set_column(i, i, column_len + 2)
+
+    return beta_MLE, X_TWX
+
+
+"""
+# Settings for each dataset in Table 1 to Table 4 (page 2581)
+n_values = [50, 100, 200]
+p_values = [4, 8]
+rho2_values = [0.90, 0.95, 0.99]
+tolerance = 1e-7
+
+base_seed = 426
+rng_seed_generator = np.random.default_rng(base_seed)
+seeds = rng_seed_generator.integers(low=0, high=50000, size=50000)
+
+# Start simulation
+setting_index = 0
+for p in p_values:
+    for rho2 in rho2_values:
+        for n in n_values:
+            current_seed = seeds[setting_index]
+            X, y, beta_true, _ = generate_asar_data(
+                n, p, rho2, seed=current_seed, setting_index=setting_index
+            )
+            print(f"\ndata {setting_index + 1}: {n}/{p}/{rho2}:")
+
+            # correlation_matrix = np.corrcoef(X, rowvar=False)
+            # print(f"\nCorrelation matrix:")
+            # print(np.round(correlation_matrix, decimals=2), "\n")
+
+            beta_MLE, X_TWX = MLE(X, y, tolerance, setting_index, save_file=True)
+
+            setting_index += 1
+
+            print(f"beta true: {beta_true}")
+            print(f"beta MLE: {beta_MLE}")
+            print("-" * 80)
+
+            # Save the data
+            data = {
+                "beta true": beta_true,
+                "beta MLE": beta_MLE,
+            }
+            df = pd.DataFrame(data)
+
+            output_dir = f"output/Asar_MLE/"
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir, exist_ok=True)
+
+            filename = f"MLE_{setting_index}.xlsx"
+            filepath = os.path.join(output_dir, filename)
+
+            with pd.ExcelWriter(filepath, engine="xlsxwriter") as writer:
+                df.to_excel(writer, index=False)
+
+                # Adjust column widths
+                worksheet = writer.sheets["Sheet1"]
+                for i, col in enumerate(df.columns):
+                    column_len = max(df[col].astype(str).map(len).max(), len(col))
+                    worksheet.set_column(i, i, column_len + 2)
+"""
